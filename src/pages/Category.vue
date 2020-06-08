5bc3e00c5aab9b7be7b775046840b98f56f9bfbf
小陈 <1716867307@qq.com>
2020-06-08 16:06:24 +0800

    分类逻辑功能的实现

--- src/pages/Category.vue
+++ src/pages/Category.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <div class="menu">
-      <div class="menu-left" ref="itemList">
+      <div class="menu-left">
         <ul>
           <li class="menu-item" v-for="(menu,index) in menus" :key="index" :class="{current: index === currentIndex}"
-            @click="clickList(index)">
+            @click="clickList(index)" ref="menuList">
             <p class="text">{{ menu.name }}</p>
           </li>
         </ul>
       </div>
-      <div class="menu-right">
+      <div class="menu-right" ref="itemList">
         <ul>
           <li class="cate" v-for="(menu, index1) in menus" :key="index1">
             <h4 class="cate-title">{{ menu.name }}</h4>
@@ -24,6 +24,7 @@
               </li>
             </ul>
           </li>
+          <li class="cate-bottom"></li>
         </ul>
       </div>
     </div>
@@ -37,8 +38,9 @@ export default {
   data () {
     return {
       menus: [],
-      currentIndex: 0,
+      // currentIndex: 0,
       rightLiTops: [],  // 右菜单每项的高度
+      scrollY: 0        // 记住右菜单的滚动距离
     }
   },
   watch: {
@@ -61,20 +63,24 @@ export default {
   },
   methods: {
     clickList (index) {
-      this.currentIndex = index
-      // scrollTo(x, y, time, easing)
-      this.rightBscroll.scrollTo(0, -this.rightLiTops[index])
+      this.scrollY = this.rightLiTops[index]
+      this.rightBscroll.scrollTo(0, -this.scrollY)
     },
     // 初始化better-scroll
     _initBScroll () {
       this.leftBscroll = new BScroll('.menu-left', {
         click: true,
-        mouseWheel: true
+        mouseWheel: true,
+        probeType: 3  // 实时派发scroll事件
       })
       this.rightBscroll = new BScroll('.menu-right', {
         click: true,
         mouseWheel: true
       })
+      this.rightBscroll.on('scroll', (pos) => {
+        this.scrollY = Math.abs(pos.y)
+      })
+
     },
     _initRightHeight () {
       let itemArray = []
@@ -86,9 +92,33 @@ export default {
         top += li.clientHeight
         itemArray.push(top)
       })
+      let bottom = this.$refs.itemList.getElementsByClassName('cate-bottom')[0]
+      bottom.style.height = this.$refs.itemList.clientHeight / 1.2 + 'px'
       this.rightLiTops = itemArray
+    },
+    _initLeftScroll (index) {
+      let menu = this.$refs.menuList
+      let el = menu[index]
+      // scrollToElement(el, time, offsetX, offsetY, easing)
+      this.leftBscroll.scrollToElement(el, 300, 0, -100)
     }
 
+  },
+  computed: {
+    currentIndex () {
+      // 当 scrollY 发生改变时，修改左菜单的激活项
+      const { scrollY, rightLiTops } = this
+      // 从右菜单中查找元素，返回元素索引
+      return rightLiTops.findIndex((top, index) => {
+        if (index === rightLiTops.length - 2) {
+          return true
+        }
+        if (scrollY >= top && scrollY < rightLiTops[index + 1]) {
+          this._initLeftScroll(index)
+          return true
+        }
+      })
+    }
   },
 }
 </script>
