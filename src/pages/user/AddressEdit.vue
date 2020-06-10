64b21c855dad8e1a8f2e22c81609eca55c7b3b6d
小陈 <1716867307@qq.com>
2020-06-10 21:03:32 +0800

    添加地区选择器

--- src/pages/user/AddressEdit.vue
+++ src/pages/user/AddressEdit.vue
@@ -10,7 +10,13 @@
     </div>
     <div class="mui-input-row">
       <label class="tit">所在地区</label>
-      <input v-model="form.area" type="text" class="mui-input-clear" placeholder="所在地区">
+      <input v-model="form.area" type="text" class="mui-input-clear" placeholder="所在地区" @click="choose">
+      <div class="divwrap">
+        <div class="mask" @click="choose" v-if="show"></div>
+        <v-distpicker v-if="show" type="mobile" @province="onChangeProvince" @city="onChangeCity" @area="onChangeArea"
+          :province="newInfo.province" :city="newInfo.city" :area="newInfo.district">
+        </v-distpicker>
+      </div>
     </div>
     <div class="mui-input-row">
       <label class="tit">详细地址</label>
@@ -23,6 +29,7 @@
 </template>
 
 <script>
+import VDistpicker from 'v-distpicker'
 export default {
   data () {
     return {
@@ -30,14 +37,50 @@ export default {
         name: '',
         tel: '',
         area: '',
-        detail: ''
+        detail: '',
+        show: false,     // 是否显示遮罩层
+        newInfo: {
+          province: '', // 省
+          city: '',     // 市
+          area: '',     // 区
+        }
       }
     }
   },
   methods: {
     save () {
-      window.console.log('确认')
-    }
+      this.$indicator.open({
+        text: '提交中'
+      })
+      this.$http.post('address/save', this.form).then(res => {
+        this.$indicator.close()
+        if (res.data.code === 0) {
+          this.$toast(res.data.msg)
+        } else if (res.data.code === 1) {
+          this.$toast(res.data.msg)
+          this.$router.go(-1)
+        } else if (res.data.code === 2) {
+          this.$router.push({ name: 'login' })
+        }
+      })
+    },
+    choose () {
+      this.show = !this.show
+    },
+    onChangeProvince (data) {
+      this.newInfo.province = data.value
+    },
+    onChangeCity (data) {
+      this.newInfo.city = data.value
+    },
+    onChangeArea (data) {
+      this.newInfo.area = data.value
+      this.form.area = this.newInfo.province + '-' + this.newInfo.city + '-' + this.newInfo.area
+      this.show = false
+    },
+  },
+  components: {
+    'v-distpicker': VDistpicker
   }
 }
 </script>
@@ -76,5 +119,26 @@ input:-moz-placeholder {
 input:-ms-input-placeholder {
   /* Internet Explorer 10-11 */
   color: #999;
+}
+.divwrap > .mask {
+  background: #000;
+  opacity: 0.3;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+.divwrap > .distpicker-address-wrapper {
+  color: #999;
+  background: #fff;
+  border-top: 1px solid #ccc;
+  z-index: 1;
+  height: 300px;
+  overflow-y: auto;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
 }
 </style>
