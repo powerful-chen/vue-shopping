c96815f5d5f580c4b1daa13ccca71ad71374b5c5
小陈 <1716867307@qq.com>
2020-06-11 13:43:18 +0800

    显示收货地址

--- src/pages/user/Address.vue
+++ src/pages/user/Address.vue
@@ -1,12 +1,54 @@
 <template>
   <div class="address-container">
+    <div>
+      <div>测试默认地址表</div>
+      <div class="mui-card" v-for="item in addressList" :key="item.id">
+        <ul class="selected">
+          <li class="title">
+            <strong>{{ item.name }}</strong>&nbsp;
+            <strong>{{ item.tel }}</strong>
+          </li>
+          <li>{{ item.area }} {{ item.detail }}</li>
+          <li>
+            <a class="edit">编辑</a>
+          </li>
+        </ul>
+      </div>
+    </div>
     <div>收货地址列表</div>
-    <div class="mod_btns fixed">
+    <div v-show="showAdd" class="mod_btns fixed">
       <router-link :to="{ name: 'address_add' }" class="mod_btn">新增收货地址</router-link>
     </div>
   </div>
 </template>
 
+
+<script>
+export default {
+  data () {
+    return {
+      addressList: [],
+      showAdd: false
+    }
+  },
+  created () {
+    this.getAddressList()
+  },
+  methods: {
+    getAddressList () {
+      this.$indicator.open({
+        text: '加载中'
+      })
+      this.$http.get('address').then(res => {
+        this.$indicator.close()
+        this.showAdd = true
+        this.addressList=res.data.data
+        window.console.log(res.data)
+      })
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .address-container {
@@ -25,6 +67,30 @@
   .mod_btns .fixed {
     display: flex;
     overflow: hidden;
+  }
+  .mui-card {
+    padding: 7px 40px;
+    position: relative;
+    overflow: hidden;
+    padding: 15px;
+    color: #666;
+    .selected {
+      margin: 0;
+      padding: 0;
+      list-style: none;
+      padding-right: 50px;
+      .title {
+        font-size: 16px;
+      }
+      .edit {
+        color: #e93b3d;
+        width: 50px;
+        position: absolute;
+        top: 50%;
+        right: 0;
+        transform: translateY(-50%);
+      }
+    }
   }
 }
 </style>
